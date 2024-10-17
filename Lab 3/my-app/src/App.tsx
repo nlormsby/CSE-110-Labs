830fab210e36bcde41a51ad12036302958ab6538
erizheng <114521575+erizheng@users.noreply.github.com>
2024-10-16 23:09:01 -0700

    copied my-app from lab2

---
+++ Lab 3/my-app/src/App.tsx
@@ -0,0 +1,102 @@
+import './App.css';
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
+import {ClickCounter, ToggleHeart, ToggleTheme} from "./hooksExercise";
+import React, { useState, useEffect, useContext } from 'react';
+import { ThemeContext, themes } from "./themeContext";
+
+
+function App() {
+  const [heartList, setHeart] = useState<string[]>([]);
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
+  const [notes, setNotes] = useState(dummyNotesList); 
+  const initialNote = {
+    id: -1,
+    title: "",
+    content: "",
+    label: Label.other,
+  };
+  const [selectedNote, setSelectedNote] = useState<Note>(initialNote);
+const [createNote, setCreateNote] = useState(initialNote);
+const createNoteHandler = (event: React.FormEvent) => {
+  event.preventDefault();
+  console.log("title: ", createNote.title);
+  console.log("content: ", createNote.content);
+  createNote.id = notes.length + 1;
+  setNotes([createNote, ...notes]);
+  setCreateNote(initialNote);
+};
+
+ return (
+  <ThemeContext.Provider value={currentTheme}>
+   <div className='app-container'>
+   <form className="note-form" onSubmit={createNoteHandler}>
+    	<div>
+      	<input
+        	placeholder="Note Title"
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, title: event.target.value })}
+        	required>
+      	</input>
+    	</div>
+
+    	<div>
+      	<textarea
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, content: event.target.value })}
+        	required>
+      	</textarea>
+    	</div>
+
+  <div>
+     	<select
+       	onChange={(event) =>
+         	setCreateNote({ ...createNote, label: event.target.value as Label})}
+       	required>
+       	<option value={Label.personal}>Personal</option>
+       	<option value={Label.study}>Study</option>
+       	<option value={Label.work}>Work</option>
+       	<option value={Label.other}>Other</option>
+     	</select>
+   	</div>
+
+    	<div><button type="submit">Create Note</button></div>
+  	</form>
+
+      <div className="notes-grid">
+       {notes.map((note) => (
+         <div
+           key={note.id}
+           className="note-item"
+           style = {{
+            background: currentTheme.background,
+            color: currentTheme.foreground,
+            display: 'flex'
+           }}>
+           <div className="notes-header">
+            <ToggleHeart title = {note.title} list={heartList} listSet={setHeart}/>
+             <button onClick = {(event) => {setNotes(notes.filter(sombrero => sombrero !== note))}}>x</button>
+           </div>
+           <h2 contentEditable = "true"> {note.title} </h2>
+           <p contentEditable = "true"> {note.content} </p>
+           <p contentEditable = "true"> {note.label} </p>
+         </div>
+       ))}
+     </div>
+     <div>
+      <h2>
+        List of favorites:
+      </h2>
+      <p>
+        {heartList.map(text => <p>{text}</p>)}
+      </p>
+     </div>
+     <div>
+           <ToggleTheme leg={currentTheme} legSet={setCurrentTheme}/>
+     </div>
+   </div>
+   </ThemeContext.Provider>
+ );
+}
+
+export default App;
---
+++ Lab 3/my-app/src/hooksExercise.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { ThemeContext, themes } from "./themeContext";
+import { stringify } from 'querystring';
+
+
+export function ClickCounter() {
+    const theme = useContext(ThemeContext);
+
+ const [count, setCount] = useState(0);
+
+ const handleClick = () => {
+   setCount(count + 1);
+ };
+
+ useEffect(() => {
+   document.title = `You clicked ${count} times`;
+ }, [count]);
+
+ return (
+    <div
+      style={{
+        background: theme.background,
+        color: theme.foreground,
+        padding: "20px",
+      }}
+    >
+      <p>You clicked {count} times </p>
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{ background: theme.foreground, color: theme.background }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+
+// Wrapper component to provide context
+
+interface chair{
+    leg:{
+      foreground: string;
+      background: string;
+    };
+    legSet: React.Dispatch<React.SetStateAction<
+    
+    {
+      
+        foreground: string;
+      
+      
+        background: string;
+    }>>;
+  }
+
+
+export function ToggleTheme(floor: chair) {
+   
+    const toggleTheme = () => {
+      floor.legSet(floor.leg === themes.light ? themes.dark : themes.light);
+    };
+   
+    return (
+      <ThemeContext.Provider value={floor.leg}>
+        <button onClick={toggleTheme}> Toggle Theme </button>
+      </ThemeContext.Provider>
+    );
+   }
+   
+   export default ToggleTheme;
+
+interface listyThing {
+    title: string;
+    list: string[];
+    listSet: React.Dispatch<React.SetStateAction<string[]>>;
+    
+}
+
+export function ToggleHeart(burrito: listyThing) {
+    const [currentStatus, setCurrentStatus] = useState("♡");
+   
+    const toggleStatus = () => {
+      setCurrentStatus(currentStatus === "♡" ? "❤️" : "♡");
+      if (burrito.list.includes(burrito.title)) {
+        burrito.listSet((prevlist) => prevlist.filter(sombrero => sombrero !== burrito.title));
+      }
+      else {
+        burrito.listSet((prevlist) => [...prevlist, burrito.title]);
+      }
+    };
+   
+    return (
+        <button onClick={toggleStatus}> {currentStatus} </button>
+    );
+}
