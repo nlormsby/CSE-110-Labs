cb663cb7503fd151c3215380dc3c697d622fbef0
nlormsby <nlormsby@gmail.com>
2024-10-16 23:30:43 -0700

    imported StickyNotes and todolist in app

--- Lab 3/my-app/src/App.tsx
+++ Lab 3/my-app/src/App.tsx
@@ -1,6 +1,17 @@
+import "./App.css";
+import { ToDoList } from "./toDoList";
+import { Route, Routes } from "react-router-dom";
+import { StickyNotes } from "./stickyNotes";
 
-function App() {
-  
-}
+const App = () => {
+ return (
+   <div>
+     <Routes>
+       <Route path="/" element={<StickyNotes />} />
+       <Route path="/todolist" element={<ToDoList />} />
+     </Routes>
+   </div>
+ );
+};
 
 export default App;
