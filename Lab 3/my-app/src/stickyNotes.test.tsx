997e0f372e8bde74ff70c15ed71fc4a684511c54
erizheng <114521575+erizheng@users.noreply.github.com>
2024-10-18 19:24:09 -0700

    delete test done

--- Lab 3/my-app/src/stickyNotes.test.tsx
+++ Lab 3/my-app/src/stickyNotes.test.tsx
@@ -5,7 +5,7 @@ test("renders create note form", () => {
  const createNoteButton = screen.getByText("Create Note");
  expect(createNoteButton).toBeInTheDocument();
 });
-describe("Create StickyNote", () => {
+describe("StickyNote", () => {
  test("renders create note form", () => {
    render(<StickyNotes />);
 
@@ -34,4 +34,54 @@ describe("Create StickyNote", () => {
    expect(newNoteTitle).toBeInTheDocument();
    expect(newNoteContent).toBeInTheDocument();
  });
+
+ //Tests
+ //read test
+//  test("read a note", () => {
+//   render(<StickyNotes />);
+
+
+
+//  });
+//  //update test
+//  test("update a note", () => {
+//   render(<StickyNotes />);
+  
+//  });
+ //delete test
+ test("delete a note", () => {
+  render(<StickyNotes />);
+
+  //before clicking x
+  const note1 = screen.queryByText("test note 1 title");
+  const note2 = screen.queryByText("test note 2 title");
+  const note3 = screen.queryByText("test note 3 title");
+  const note4 = screen.queryByText("test note 4 title");
+  const note5 = screen.queryByText("test note 5 title");
+  const note6 = screen.queryByText("test note 6 title");
+  //they should all be in the document
+  expect(note1).toBeInTheDocument();
+  expect(note2).toBeInTheDocument();
+  expect(note3).toBeInTheDocument();
+  expect(note4).toBeInTheDocument();
+  expect(note5).toBeInTheDocument();
+  expect(note6).toBeInTheDocument();
+
+
+  //clicking all the x to delete them all
+  const xButton = screen.getAllByText('x');
+  xButton.forEach((element) => {fireEvent.click(element);});
+
+  //all dummy notes should be gone since x is pressed for them all
+  expect(note1).not.toBeInTheDocument();
+  expect(note2).not.toBeInTheDocument();
+  expect(note3).not.toBeInTheDocument();
+  expect(note4).not.toBeInTheDocument();
+  expect(note5).not.toBeInTheDocument();
+  expect(note6).not.toBeInTheDocument();
+
+
+  
+ });
+
 });
